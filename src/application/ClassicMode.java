466ab2b7975e744a0a7c194c3b3f4e0d84f9d7a9
Dolwijit <dol51890@gmail.com>
2018-11-07 23:42:37 +0700

    fix level.txt

--- src/application/ClassicMode.java
+++ src/application/ClassicMode.java
@@ -5,17 +5,20 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class ClassicMode extends Mode {
+public class ClassicMode extends Mode
+{
 
 	private static String levelFile = "level.txt";
 	private static String[] levels;
 
-	public ClassicMode(int level) {
+	public ClassicMode(int level)
+	{
 		String[] start = levels[level - 1].split(" ");
 		level--;
 		int n = 5 + level / 5;
 		Board board = new Board(n, 2);
-		for (int i = 0; i < start.length; i++) {
+		for (int i = 0; i < start.length; i++)
+		{
 			int temp = Integer.parseInt(start[i]);
 			board.changeColor(temp / n, temp % n, true);
 		}
@@ -23,22 +26,32 @@ public class ClassicMode extends Mode {
 		getChildren().addAll(board, gameMenu);
 	}
 
-	public static void readLevel() {
+	public static void readLevel()
+	{
 		File file = null;
 		BufferedReader fileReader = null;
 		String thisLine = null;
-		try {
+		try
+		{
 			file = new File(levelFile);
 			fileReader = new BufferedReader(new FileReader(file));
-			while ((thisLine = fileReader.readLine()) != null) {
+			while ((thisLine = fileReader.readLine()) != null)
+			{
 				levels = thisLine.split("-");
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			e.printStackTrace();
-		} finally {
-			try {
+		}
+		finally
+		{
+			try
+			{
 				fileReader.close();
-			} catch (IOException e) {
+			}
+			catch (IOException e)
+			{
 				e.printStackTrace();
 			}
 		}
